38eaf8b9c1e276780ade3e114e813bcc6705d57c
eli zabeth <mutaeli@gmail.com>
2018-04-11 16:04:23 +0200

    filtre titre

--- lib/func.wordpress.php
+++ lib/func.wordpress.php
@@ -3,3 +3,18 @@
 
 //Enlever la barre Admin en front
 // show_admin_bar( false );
+//get the title renvoie  return apply_filters( 'the_title', $title, $id );
+ 
+
+function filtre_sur_titre($titre){
+    $prefixe = get_field ('prefixe');
+    $couleurPrefixe = get_field ('couleur_prefixe');
+    if (! is_singular( 'product' )){  //si ce n'est pas la fiche produit
+        return  $titre;
+    } 
+
+
+    return sprintf('<span style="color:%s;">%s </span> %s', $couleurPrefixe, $prefixe, $titre);
+}
+
+add_filter( 'the_title', 'filtre_sur_titre');
