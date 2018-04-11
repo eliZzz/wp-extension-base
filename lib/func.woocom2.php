5d7d48b8edd28d6d37a6b3832bde3b455799bf6a
eli zabeth <mutaeli@gmail.com>
2018-04-11 16:30:57 +0200

    ordre des onglets

--- lib/func.woocom2.php
+++ lib/func.woocom2.php
@@ -8,10 +8,17 @@ function eli_onglets_fiche_produit($tabs){
     // unset($tabs['description']);
     //Les trois onglets par defaut sont description, reviews, additional information
     
+    //renommer un onglet
     $tabs['description']['title'] = 'Zoom';
 
+
+    //changer l'ordre
+    $tabs['description']['priority'] = 2;
+    $tabs['reviews']['priority'] = 1;
 
     return $tabs;
 }
 
 add_filter('woocommerce_product_tabs', 'eli_onglets_fiche_produit');
+
+
