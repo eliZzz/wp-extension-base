f47d97d0f491a085687dfac73119243e535711aa
eli zabeth <mutaeli@gmail.com>
2018-04-11 16:41:19 +0200

    ajout onglet sur mesure

--- lib/func.woocom2.php
+++ lib/func.woocom2.php
@@ -16,7 +16,19 @@ function eli_onglets_fiche_produit($tabs){
     $tabs['description']['priority'] = 2;
     $tabs['reviews']['priority'] = 1;
 
+    $tabs['composition_textile'] = array(
+        'title' => 'Composition Textile',
+        'priority' => 3,
+        'callback' => 'eli_onglet_sur_mesure'
+    );
+
     return $tabs;
+}
+
+function eli_onglet_sur_mesure(){
+    $html = '<h2>Titre de mon onglet</h2>' ;
+    $html .= '<p>Le contentu de mon onglet</p>';
+    echo $html;
 }
 
 add_filter('woocommerce_product_tabs', 'eli_onglets_fiche_produit');
