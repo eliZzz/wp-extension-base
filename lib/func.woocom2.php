aee2b161649c2169a9d3481e59af7e5e06021dd9
eli zabeth <mutaeli@gmail.com>
2018-04-11 16:24:51 +0200

    renommer un onglet

--- lib/func.woocom2.php
+++ lib/func.woocom2.php
@@ -5,8 +5,12 @@
 
 function eli_onglets_fiche_produit($tabs){
     //retirer un onglet
+    // unset($tabs['description']);
     //Les trois onglets par defaut sont description, reviews, additional information
-    unset($tabs['description']);
+    
+    $tabs['description']['title'] = 'Zoom';
+
+
     return $tabs;
 }
 
