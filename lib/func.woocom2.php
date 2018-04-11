6c00e0e45a013b8f5c4679e6f1bdafec250e0a2b
eli zabeth <mutaeli@gmail.com>
2018-04-11 16:21:00 +0200

    Ajout filtre, onglet description retiré

--- lib/func.woocom2.php
+++ lib/func.woocom2.php
@@ -3,9 +3,11 @@
 # Personnalisation des onglets de la fiche produit
 
 
-function eli_onglets_fiche_produit(){
-
-    
+function eli_onglets_fiche_produit($tabs){
+    //retirer un onglet
+    //Les trois onglets par defaut sont description, reviews, additional information
+    unset($tabs['description']);
+    return $tabs;
 }
 
 add_filter('woocommerce_product_tabs', 'eli_onglets_fiche_produit');
